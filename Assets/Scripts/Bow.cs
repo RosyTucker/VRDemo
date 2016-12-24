a68e61b28c4a4a491bc67a0a90f771d068b9f674
Rose Tucker <rosy533@gmail.com>
2016-12-24 18:05:21 +0000

    Initial commit

---
+++ Assets/Scripts/Bow.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bow : MonoBehaviour {
+
+    public SteamVR_TrackedObject TrackedObject;
+
+    private SteamVR_Controller.Device _controller { get { return SteamVR_Controller.Input((int)TrackedObject.index); } }
+ 
+    // Use this for initialization
+    void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        gameObject.transform.position = _controller.transform.pos;
+        gameObject.transform.localRotation = _controller.transform.rot;
+    }
+}
